7549b24150eb8a7e437f05b8a830c4943fafa4cc
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-06-18 13:13:43 +0200

    create class for endpoints

--- src/test/java/api/endpoints/UserEndPoints.java
+++ src/test/java/api/endpoints/UserEndPoints.java
@@ -1,2 +1,60 @@
-package api.endpoints;public class UserEndPoints {
+package api.endpoints;
+import api.payload.User;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+// Perform CRUD Operations for User API
+
+public class UserEndPoints {
+
+    public static Response createUser(User payload){
+
+           Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+
+                    .when()
+                .post(Routes.post_url);
+
+           return response;
+    }
+
+    public static Response ReadUser(String userName){
+
+        Response response = given()
+                .pathParam("username", userName)
+
+                .when()
+                .get(Routes.get_url);
+
+        return response;
+    }
+
+    public static Response UpdateUser(String userName, User payload){
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .pathParam("username", userName)
+                .body(payload)
+
+                .when()
+                .put(Routes.update_url);
+
+        return response;
+    }
+
+    public static Response DeleteUser(String userName){
+
+        Response response = given()
+                .pathParam("username", userName)
+
+                .when()
+                .put(Routes.delete_url);
+
+            return response;
+    }
+
 }
