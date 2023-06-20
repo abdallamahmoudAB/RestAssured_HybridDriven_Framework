57252fbbf051056f7a93da9de485371be75099ab
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-06-20 06:21:33 +0200

    create dataDrivenTest class

--- src/test/java/api/test/DataDrivenTest.java
+++ src/test/java/api/test/DataDrivenTest.java
@@ -1,2 +1,37 @@
-package api.test;public class DataDrivenTest {
+package api.test;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import api.utilities.DataProviders;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DataDrivenTest {
+
+    @Test(priority = 1, dataProvider = "Data", dataProviderClass = DataProviders.class)
+    public void postUser(String userID, String userName, String firstName, String lastName, String Email, String Password, String Phone){
+
+        User userPayload = new User();
+
+        userPayload.setId(Integer.parseInt(userID));
+        userPayload.setUsername(userName);
+        userPayload.setFirstName(firstName);
+        userPayload.setLastName(lastName);
+        userPayload.setEmail(Email);
+        userPayload.setPassword(Password);
+        userPayload.setPhone(Phone);
+
+        Response response = UserEndPoints.createUser(userPayload);
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test(priority = 2, dataProvider = "UserNames", dataProviderClass = DataProviders.class)
+    public void DeleteUserByName(String userName){
+
+        Response response = UserEndPoints.DeleteUser(userName);
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+    }
+
 }
